aa25164926db23331db42ae2699df2e73448d271
lucas.valenzuela <lucas.valenzuela@L3-000278>
2021-12-21 12:01:03 -0300

    Add project files.

---
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        public List<Aluno> Alunos = new List<Aluno>()
+        {
+            new Aluno(){ Id = 1, Nome = "Marcos",Sobrenome="Almeida", Telefone= "993537517"},
+            new Aluno(){ Id = 2, Nome = "Marta", Sobrenome="Kent", Telefone= "993537518"},
+            new Aluno(){ Id = 3, Nome = "Laura", Sobrenome="Maria", Telefone= "993537519"}
+        };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(x => x.Id == id);
+
+            if (aluno == null)
+                return BadRequest("O aluno não foi encontrado");
+
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, [FromBody] Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
